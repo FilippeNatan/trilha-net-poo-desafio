125652c4debdf4c20e07f811fff8f4c744fb0da6
FilippeTaggie <filippe.xavier@taggie.com.br>
2024-02-17 19:58:22 -0300

    feat: Resolução de desafio

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,11 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria){}
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine(String.Format("O aplicativo {0} está sendo instalado no seu dispositivo Android...", nomeApp));
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("1198765432", "3220", "122333444455555", 1024);
+Iphone iphone = new Iphone("9912345678", "Fortin", "988777666655555", 512);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Uber");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Ifood");
